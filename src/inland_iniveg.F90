bb0c28ea9143c509f58c5c37e055db76b0c81de5
Vitor Fontes <vitor.ufv2005@gmail.com>
2018-08-07 17:48:04 -0300

    Corrigindo distribuicao inicial de lai dentro do iniveg para os pfts 11 e 12

--- src/inland_iniveg.F90
+++ src/inland_iniveg.F90
@@ -264,14 +264,9 @@ subroutine iniveg (isimveg, irestart)
                plai(i,9)  = exist(i,9)  / float(ishrub) * plai_init(3,inveg)
                plai(i,10) = exist(i,10) / float(ishrub) * plai_init(3,inveg)
 
-               if ((inveg.eq.9).or.(inveg.eq.10)) then
-                  if (tw(i).gt.22.0) then
+               if (((inveg.eq.9).or.(inveg.eq.10)).and.(nint(exist(i,11)).eq.1)) then
                      plai(i,11) = exist(i,11) * 0.80 * plai_init(4,inveg)
                      plai(i,12) = exist(i,12) * 0.20 * plai_init(4,inveg)
-                  else
-                     plai(i,11) = exist(i,11) * 0.00 * plai_init(4,inveg)
-                     plai(i,12) = exist(i,12) * 1.00 * plai_init(4,inveg)
-                  endif
                else
                   plai(i,11) = exist(i,11) / float(igrass) * plai_init(4,inveg)
                   plai(i,12) = exist(i,12) / float(igrass) * plai_init(4,inveg)
